03e5ec3db52ce00f36e4289a5722459d36bcb92e
Artem3124 <mr.zhukovsky02@gmail.com>
2025-01-25 10:38:10 +0100

    wip

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,10 +1,17 @@
 #include "MainComponent.h"
 
+#include "juce_graphics/juce_graphics.h"
+
 //==============================================================================
 MainComponent::MainComponent() {
   setSize(600, 400);
 
   addAndMakeVisible(playButton);
+  addAndMakeVisible(stopButton);
+  addAndMakeVisible(volumeSlider);
+  playButton.addListener(this);
+  stopButton.addListener(this);
+  volumeSlider.addListener(this);
 }
 
 MainComponent::~MainComponent() {}
@@ -12,14 +19,13 @@ MainComponent::~MainComponent() {}
 //==============================================================================
 void MainComponent::paint(juce::Graphics &g) {
   //
-  // (Our component is opaque, so we must completely fill the background with a
-  // solid colour)
-  g.fillAll(
-      getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+  // (Our component is opaque, so we must completely fill the background
+  // with a solid colour)
+  g.fillAll(juce::Colours::white);
 
   g.setFont(juce::FontOptions(16.0f));
-  g.setColour(juce::Colours::white);
-  g.drawText("Hello World!", getLocalBounds(), juce::Justification::centred,
+  g.setColour(juce::Colours::black);
+  g.drawText("Hello World!", getLocalBounds(), juce::Justification::bottomLeft,
              true);
 }
 
@@ -28,7 +34,33 @@ void MainComponent::resized() {
   // If you add any child components, this is where you should
   // update their positions.
   int rowH = getHeight() / 5;
-  playButton.setBounds(0, 0, getWidth(), rowH);
+  int closeBthH = getHeight() / 7; 
 
   playButton.setBounds(0, rowH, getWidth(), rowH);
+  stopButton.setBounds(2 * (getWidth() /3), 3 * rowH, getWidth() / 3, closeBthH);
+  volumeSlider.setBounds(0, rowH + rowH, getWidth(), rowH);
+}
+
+
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& buffer) {
+    // keep on going here
+}
+
+/** Called when the button is clicked. */
+void MainComponent::buttonClicked(juce::Button *button) {
+    if (button == &playButton) {
+        std::cout << "lol the button is working" << std::endl;
+        return;
+    }
+
+    if (button == &stopButton) {
+        std::cout << "lol the button stop is working" << std::endl;
+        return;
+    }
+
+    return;
+}
+
+void MainComponent::sliderValueChanged (juce::Slider *slider) {
+    std::cout << "aint no way the slider is working" << volumeSlider.getValue() << std::endl;
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,15 +1,20 @@
 #pragma once
 
-#include "juce_gui_basics/juce_gui_basics.h"
 #include <JuceHeader.h>
+
+#include "juce_core/juce_core.h"
+#include "juce_gui_basics/juce_gui_basics.h"
+#include "juce_audio_basics/sources/juce_AudioSource.h"
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent : public juce::Component {
-public:
+class MainComponent : public juce::Component,
+                      public juce::Button::Listener,
+                      public juce::Slider::Listener {
+ public:
   //==============================================================================
   MainComponent();
   ~MainComponent() override;
@@ -17,11 +22,19 @@ public:
   //==============================================================================
   void paint(juce::Graphics &) override;
   void resized() override;
+  void buttonClicked(juce::Button *button) override;
+  void sliderValueChanged(juce::Slider *) override;
+  void getNextAudioBlock(const juce::AudioSourceChannelInfo& buffer);
 
-private:
+ private:
   //==============================================================================
   // Your private member variables go here...
   juce::TextButton playButton{"Hello honey"};
+  juce::TextButton stopButton{"Bye"};
+  juce::Slider volumeSlider{juce::Slider::SliderStyle::LinearHorizontal,
+                      juce::Slider::TextEntryBoxPosition::TextBoxLeft};
+
+  juce::Random rand;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
